3410f5f68f5420430490d4dd18824f2c22145db6
WOOJAE JO <63555689+Jo-dv@users.noreply.github.com>
2024-08-04 22:04:29 +0900

    Title: 아름다운 수, Time: 515ms, Memory: 29MB, Status: Passed - CodeTree

---
+++ 240804/아름다운 수/beautiful-number.py
@@ -0,0 +1,28 @@
+n = int(input())
+answer = 0
+
+def check(num):
+    i = 0
+    while i < len(num):
+        cnt = 1
+        while i + 1 < n and num[i] == num[i + 1]:
+            i += 1
+            cnt += 1
+        if cnt % int(num[i]) != 0:
+            return False
+        i += 1
+    return True
+
+def dfs(num):
+    global answer
+
+    if len(num) == n:
+        if check(num):
+            answer += 1
+        return
+
+    for i in range(1, 5):
+        dfs(num + str(i))
+    
+dfs("")
+print(answer)
