a195111ab63a2f15bbadcb08e274b2969cecfe30
WOOJAE JO <63555689+Jo-dv@users.noreply.github.com>
2024-08-11 21:28:40 +0900

    Title: 겹치지 않게 선분 고르기, Time: 105ms, Memory: 24MB, Status: Wrong Answer - CodeTree

---
+++ 240811/겹치지 않게 선분 고르기/select-segments-without-overlap.py
@@ -0,0 +1,21 @@
+n = int(input())
+lines = [tuple(map(int, input().split())) for _ in range(n)]
+answer = 0
+selected_lines = []
+
+def search(idx):
+    global answer
+    if idx == n:
+        for i, line1 in enumerate(selected_lines):
+            for j, line2 in enumerate(selected_lines):
+                if j > i and(line1[1] < line2[0] or line2[1] < line1[0]):  # 겹치지 않은 경우 
+                    answer = max(answer, len(selected_lines))
+        return
+
+    selected_lines.append(lines[idx])    
+    search(idx + 1)
+    selected_lines.pop()
+    search(idx + 1)
+
+search(0)
+print(answer)
