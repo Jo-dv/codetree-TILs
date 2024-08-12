0ea77cc5e1b5d2c8d8a26adcc8bb083b4d574eed
WOOJAE JO <63555689+Jo-dv@users.noreply.github.com>
2024-08-12 19:45:12 +0900

    Title: 뿌요뿌요, Time: 172ms, Memory: 26MB, Status: Passed - CodeTree

--- 240812/뿌요뿌요/puyo-puyo.py
+++ 240812/뿌요뿌요/puyo-puyo.py
@@ -1,5 +1,6 @@
 n = int(input())
 grid = [list(map(int, input().split())) for _ in range(n)]
+visited = [[False] * n for _ in range(n)]
 block = 0
 max_block_size = 0
 
@@ -7,7 +8,6 @@ def search(y, x, target):
     global current_block_size
     
     if 0 <= y < n and 0 <= x < n and not visited[y][x] and grid[y][x] == target:
-        # block_list.append((y, x))
         visited[y][x] = True
         current_block_size += 1
         
@@ -18,25 +18,8 @@ def search(y, x, target):
         return True
     return False
 
-# def explode():
-#     global block
-
-#     block += 1
-#     for y, x in block_list:
-#         grid[y][x] = 0
-
-# def down_block():
-#     for x in range(n):
-#         for i in range(n - 2, -1, -1):  # 반복됨에 따라 천장의 높이를 높여나감
-#             for y in range(n - 1, i, -1):
-#                 if grid[i][x] != 0 and grid[y][x] == 0:  # 현재 천장(i)에 내릴 블록이 있고, 현재 y는 빈 곳이라면
-#                     grid[y][x] = grid[i][x]
-#                     grid[i][x] = 0
-
 for i in range(n):
     for j in range(n):
-        block_list = []
-        visited = [[False] * n for _ in range(n)]
         current_block_size = 0
         if search(i, j, grid[i][j]):
             max_block_size = max(max_block_size, current_block_size)
