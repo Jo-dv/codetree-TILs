3428730243a86fa616a4643a226fd16c34acb4f9
WOOJAE JO <63555689+Jo-dv@users.noreply.github.com>
2024-08-25 22:53:03 +0900

    Title: 앞에서부터 삭제하기 2, Time: 5025ms, Memory: 30MB, Status: Time Limit Exceed - CodeTree

--- 240825/앞에서부터 삭제하기 2/delete-it-from-the-beginning-2.py
+++ 240825/앞에서부터 삭제하기 2/delete-it-from-the-beginning-2.py
@@ -7,7 +7,7 @@ nums = list(map(int, input().split()))
 answer = 0.
 heapq.heapify(nums)
 
-for k in range(1, n - 1):
+for k in range(n - 2):
     heapq.heappop(nums)
     answer = max(answer, sum(nums) / len(nums))
 
