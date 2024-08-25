42157b8090703ae10f5566f80a57d97dabf8d887
WOOJAE JO <63555689+Jo-dv@users.noreply.github.com>
2024-08-25 22:40:21 +0900

    Title: 앞에서부터 삭제하기 2, Time: 118ms, Memory: 24MB, Status: Wrong Answer - CodeTree

---
+++ 240825/앞에서부터 삭제하기 2/delete-it-from-the-beginning-2.py
@@ -0,0 +1,16 @@
+import heapq
+
+hq = []
+
+n = int(input())
+nums = list(map(int, input().split()))
+answer = 0
+
+for k in range(1, n - 1):
+    removed_nums = nums[k:]
+    temp = removed_nums.copy()
+    heapq.heapify(temp)
+    heapq.heappop(temp)
+    answer = max(answer, round(sum(temp) / len(temp), 3))
+
+print(answer)
