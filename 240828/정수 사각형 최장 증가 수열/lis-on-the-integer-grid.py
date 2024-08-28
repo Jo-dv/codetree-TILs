ef9243ef14fed1d39b239ae8a25d0c05240e7082
WOOJAE JO <63555689+Jo-dv@users.noreply.github.com>
2024-08-28 17:43:15 +0900

    Title: 정수 사각형 최장 증가 수열, Time: 786ms, Memory: 54MB, Status: Passed - CodeTree

--- 240828/정수 사각형 최장 증가 수열/lis-on-the-integer-grid.py
+++ 240828/정수 사각형 최장 증가 수열/lis-on-the-integer-grid.py
@@ -1,11 +1,11 @@
 n = int(input())
 arr = [list(map(int, input().split())) for _ in range(n)]
 dp = [[1] * n for _ in range(n)]
-blocks = [(i, j) for j in range(n) for i in range(n)]  # 값, 좌표
+blocks = sorted([(arr[i][j], i, j) for j in range(n) for i in range(n)])  # 값, 좌표
 directions = [(-1, 0), (1, 0), (0, -1), (0, 1)]
 answer = 0
 
-for y, x in blocks:
+for _, y, x in blocks:
     for d in directions:
         my = y + d[0]
         mx = x + d[1]
