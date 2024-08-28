1f02675d3ed261ff670dd57d8be6e3bf6b471d89
WOOJAE JO <63555689+Jo-dv@users.noreply.github.com>
2024-08-28 11:43:49 +0900

    Title: 사각형 채우기 2, Time: 107ms, Memory: 24MB, Status: Passed - CodeTree

--- 240828/사각형 채우기 2/rectangle-fill-2.py
+++ 240828/사각형 채우기 2/rectangle-fill-2.py
@@ -2,9 +2,8 @@ n = int(input())
 dp = [0] * 1001
 dp[0] = 1
 dp[1] = 1
-dp[2] = 3
 
-for i in range(3, 1001):
+for i in range(2, 1001):
     dp[i] = (dp[i - 1] + dp[i - 2] * 2) % 10007
 
 print(dp[n])
