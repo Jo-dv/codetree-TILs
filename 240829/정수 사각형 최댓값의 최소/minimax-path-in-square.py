5bd4ed1688205ef350ed1b85f89872cc3ab48e26
WOOJAE JO <63555689+Jo-dv@users.noreply.github.com>
2024-08-29 19:29:34 +0900

    Title: 정수 사각형 최댓값의 최소, Time: 164ms, Memory: 28MB, Status: Passed - CodeTree

--- 240829/정수 사각형 최댓값의 최소/minimax-path-in-square.py
+++ 240829/정수 사각형 최댓값의 최소/minimax-path-in-square.py
@@ -12,8 +12,8 @@ for i in range(1, n):
 for i in range(1, n):
     for j in range(1, n):
         dp[i][j] = max(min(dp[i - 1][j], dp[i][j - 1]), arr[i][j])
-
-# for i in dp:
-#     print(i)
+        # 지나온 길에서 만난 최댓값 중 가장 작은 값을 찾는 문제
+        # dp[i - 1][j], dp[i][j - 1] 해당 값들은 직전의 값으로 최대일 것이므로 이 중 최소를 선택
+        # 그 중 최종적으로 최댓값을 선택
 
 print(dp[-1][-1])
