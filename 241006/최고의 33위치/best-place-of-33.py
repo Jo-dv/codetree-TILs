571c9a961f8d14ff5df4c09ee0691bf4c856c41a
WOOJAE JO <63555689+Jo-dv@users.noreply.github.com>
2024-10-06 16:15:45 +0900

    Title: 최고의 33위치, Time: 115ms, Memory: 24MB, Status: Wrong Answer - CodeTree

---
+++ 241006/최고의 33위치/best-place-of-33.py
@@ -0,0 +1,12 @@
+n = int(input())
+grid = [list(map(int, input().split())) for _ in range(n)]
+answer = 0
+
+for y in range(n - 3 + 1):
+    for x in range(n - 3 + 1):
+        check = 0
+        for i in range(3):
+            check += sum(grid[y + i][:x + 3])
+        answer = max(answer, check)
+
+print(answer)
