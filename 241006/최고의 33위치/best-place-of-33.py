8763a30d6771a356ef75ddf053f8df1d322f37eb
WOOJAE JO <63555689+Jo-dv@users.noreply.github.com>
2024-10-06 16:19:19 +0900

    Title: 최고의 33위치, Time: 154ms, Memory: 25MB, Status: Passed - CodeTree

--- 241006/최고의 33위치/best-place-of-33.py
+++ 241006/최고의 33위치/best-place-of-33.py
@@ -6,7 +6,7 @@ for y in range(n - 3 + 1):
     for x in range(n - 3 + 1):
         check = 0
         for i in range(3):
-            check += sum(grid[y + i][:x + 3])
+            check += sum(grid[y + i][x:x + 3])
         answer = max(answer, check)
 
 print(answer)
