75b2d6c147b78dcee3fd433ce81db3ee3c1d1378
WOOJAE JO <63555689+Jo-dv@users.noreply.github.com>
2024-10-07 20:26:02 +0900

    Title: 1차원 바람, Time: 180ms, Memory: 26MB, Status: Passed - CodeTree

--- 241007/1차원 바람/The-1D-wind-blows.py
+++ 241007/1차원 바람/The-1D-wind-blows.py
@@ -5,13 +5,13 @@ command = [input().split() for _ in range(q)]
 def wind(r, d):
     if d == 1:
         temp = building[r][-1]
-        for i in range(m - 1, 0, -1):
-            building[r][i] = building[r][i - 1]
+        for c in range(m - 1, 0, -1):
+            building[r][c] = building[r][c - 1]
         building[r][0] = temp
     else:
         temp = building[r][0]
-        for i in range(m - 1):
-            building[r][i] = building[r][i + 1]
+        for c in range(m - 1):
+            building[r][c] = building[r][c + 1]
         building[r][-1] = temp
 
 for cmd in command:
@@ -21,19 +21,32 @@ for cmd in command:
     wind(r, d)
 
     up_d = d
+    flag = False
     for i in range(r, 0, -1):
         for j in range(m):
             if building[i][j] == building[i - 1][j]:
                 up_d *= -1
                 wind(i - 1, up_d)
+                flag = True
                 break
+        else:
+            flag = False
+        if not flag:
+            break
+
     down_d = d
+    flag = False
     for i in range(r, n - 1):
         for j in range(m):
             if building[i][j] == building[i + 1][j]:
                 down_d *= -1
                 wind(i + 1, down_d)
+                flag = True
                 break
+        else:
+            flag = False
+        if not flag:
+            break
 
 for i in building:
     print(*i)
