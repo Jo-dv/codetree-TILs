05fa59c6c32e824139408ae0ba4eab7a250806cf
WOOJAE JO <63555689+Jo-dv@users.noreply.github.com>
2024-10-07 19:45:43 +0900

    Title: 1차원 바람, Time: -1ms, Memory: -1MB, Status: Runtime Error - CodeTree

--- 241007/1차원 바람/The-1D-wind-blows.py
+++ 241007/1차원 바람/The-1D-wind-blows.py
@@ -1,10 +1,6 @@
 n, m, q = map(int, input().split())
 building = [list(map(int, input().split())) for _ in range(n)]
-r, d = input().split()
-r = int(r) - 1
-d = 1
-# d == 1: L
-# d == -1: R
+command = [input().split() for _ in range(q)]
 
 def wind(r, d):
     if d == 1:
@@ -18,22 +14,26 @@ def wind(r, d):
             building[r][i] = building[r][i + 1]
         building[r][-1] = temp
 
-wind(r, d)
+for cmd in command:
+    r, d = cmd
+    r -= 1
+    d = 1 if d == 'L' else - 1
+    wind(r, d)
 
-up_d = d
-for i in range(r, 0, -1):
-    for j in range(m):
-        if building[i][j] == building[i - 1][j]:
-            up_d *= -1
-            wind(i - 1, up_d)
-            break
-down_d = d
-for i in range(r, n - 1):
-    for j in range(m):
-        if building[i][j] == building[i + 1][j]:
-            down_d *= -1
-            wind(i + 1, down_d)
-            break
+    up_d = d
+    for i in range(r, 0, -1):
+        for j in range(m):
+            if building[i][j] == building[i - 1][j]:
+                up_d *= -1
+                wind(i - 1, up_d)
+                break
+    down_d = d
+    for i in range(r, n - 1):
+        for j in range(m):
+            if building[i][j] == building[i + 1][j]:
+                down_d *= -1
+                wind(i + 1, down_d)
+                break
 
 for i in building:
     print(*i)
