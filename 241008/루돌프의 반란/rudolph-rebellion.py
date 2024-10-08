7cfc7420b3fa506ec15c88185acb18cdf5bb0fef
WOOJAE JO <63555689+Jo-dv@users.noreply.github.com>
2024-10-08 16:36:52 +0900

    Title: 루돌프의 반란, Time: -1ms, Memory: -1MB, Status: Runtime Error - CodeTree

--- 241008/루돌프의 반란/rudolph-rebellion.py
+++ 241008/루돌프의 반란/rudolph-rebellion.py
@@ -4,7 +4,10 @@ class Main:
     def __init__(self):
         self.n, self.m, self.p, self.c, self.d = map(int, input().split())
         self.y, self.x = map(int, input().split())
-        self.santas = [[0, 0, 0]] + [list(map(int, input().split())) for _ in range(self.p)]
+        self.santas = [[0, 0] for _ in range(self.p + 1)]
+        for _ in range(1, self.p + 1):
+            santa, sy, sx = list(map(int, input().split()))
+            self.santas[santa][0], self.santas[santa][1] = sy, sx
         self.board = [[0] * (self.n + 2) for _ in range(self.n + 2)]
         self.scores = [0] * (self.p + 1)  # 점수
         self.alive = [True] * (self.p + 1)  # 생존 여부
@@ -17,65 +20,67 @@ class Main:
     def find_santa(self):
         distances = []
         min_distance = 2 * self.n**2
-        for i in range(1, self.p + 1):  # 가장 가까운 산타 찾기
-            if not self.alive[i]:
+        for santa in range(1, self.p + 1):  # 가장 가까운 산타 찾기
+            if not self.alive[santa]:
                 continue
-            _, sy, sx = self.santas[i]
+            sy, sx = self.santas[santa]
             distance = self.cal_distance(self.y, self.x, sy, sx)
-            if distance <= min_distance:
+            if distance < min_distance:
                 min_distance = distance
-                distances.append((i, sy, sx))
-            distances.sort(key=lambda x: (-x[1], -x[2]))
+                distances = [(sy, sx, santa)]
+            elif distance == min_distance:
+                distances.append((sy, sx, santa))
+        distances.sort(reverse=True)
 
         return distances[0]
 
     def move_rudolph(self, turn):
         closest_santa = self.find_santa()
-        santa, sy, sx = closest_santa
+        sy, sx, santa = closest_santa
 
         dy, dx = 0, 0
         if self.y < sy:
             dy = 1
         elif self.y > sy:
-            dy -= 1
+            dy = -1
 
         if self.x < sx:
-            dx += 1
+            dx = 1
         elif self.x > sx:
-            dx -= 1
+            dx = -1
 
         self.board[self.y][self.x] = 0
         self.y, self.x = self.y + dy, self.x + dx
         self.board[self.y][self.x] = -1
-            
-        if self.y == sy and self.x == sx:  # 이동한 곳에 산타가 있다면
+
+        if self.y == sy and self.x == sx:  # 루돌프가 산타와 충돌
             self.back_santa(santa, sy, sx, dy, dx, self.c)  # 산타 밀기
             self.stun[santa] = turn + 2
             self.scores[santa] += self.c
 
     def back_santa(self, santa, sy, sx, dy, dx, toward):
-        dq = deque([(santa, sy, sx, dy, dx, toward)])
+        dq = deque([(santa, sy, sx, toward)])
 
         while dq:
-            santa, sy, sx, dy, dx, toward = dq.popleft()
+            santa, sy, sx, toward = dq.popleft()
             my, mx = sy + (dy * toward), sx + (dx * toward)
             if 1 <= my <= self.n and 1 <= mx <= self.n:
                 if self.board[my][mx] == 0:  # 빈칸이면
-                    self.santas[santa] = [santa, my, mx]
                     self.board[my][mx] = santa
+                    self.santas[santa] = [my, mx]
                     return
                 else:
-                    dq.append((self.board[my][mx], my, mx, dy, dx, 1))
-                    self.santas[santa] = [santa, my, mx]
+                    dq.append((self.board[my][mx], my, mx, 1))
                     self.board[my][mx] = santa
+                    self.santas[santa] = [my, mx]
             else:  # 보드를 벗어나면
                 self.alive[santa] = False
                 return
 
     def move_santa(self, turn, santa):
-        _, sy, sx = self.santas[santa]
+        sy, sx = self.santas[santa]
         distances = []
-        min_distance = 2 * self.n ** 2
+        min_distance = self.cal_distance(self.y, self.x, sy, sx)
 
         for dy, dx in ((-1, 0), (0, 1), (1, 0), (0, -1)):
             my, mx = sy + dy, sx + dx
@@ -84,28 +89,30 @@ class Main:
                 min_distance = distance
                 distances.append((my, mx, dy, dx))
 
-        if len(distances) == 0:
-            return
-        my, mx, dy, dx = distances[-1]  # 마지막으로 저장된 좌표가 가장 가까운 좌표(조건문상 계속 최소 거리일 때 활성화되므로)
-        if my == self.y and mx == self.x:
-            self.board[sy][sx] = 0
-            self.back_santa(santa, my, mx, -dy, -dx, self.d)
-            self.stun[santa] = turn + 2
-            self.scores[santa] += self.d
-        else:
-            self.board[sy][sx] = 0
-            self.board[my][mx] = santa
-            self.santas[santa] = [santa, my, mx]
+        if len(distances) != 0:
+            my, mx, dy, dx = distances[-1]  # 마지막으로 저장된 좌표가 가장 가까운 좌표(조건문상 계속 최소 거리일 때 활성화되므로)
+            if my == self.y and mx == self.x:
+                self.board[sy][sx] = 0
+                self.back_santa(santa, my, mx, -dy, -dx, self.d)
+                self.stun[santa] = turn + 2
+                self.scores[santa] += self.d
+            else:
+                self.board[sy][sx] = 0
+                self.board[my][mx] = santa
+                self.santas[santa] = [my, mx]
 
     def init_board(self):
-        for i in range(1, self.p + 1):  # 산타 위치
-            _, y, x = self.santas[i]
-            self.board[y][x] = i
+        for santa in range(1, self.p + 1):  # 산타 위치
+            sy, sx = self.santas[santa]
+            self.board[sy][sx] = santa
 
         self.board[self.y][self.x] = -1
 
     def solve(self):
         self.init_board()
+        # for i in self.board:
+        #     print(i)
+        # print()
         for turn in range(1, self.m + 1):
             if self.alive.count(True) == 0:
                 break
@@ -121,11 +128,12 @@ class Main:
                 if self.alive[santa]:
                     self.scores[santa] += 1
 
-        # for i in self.board:  # 디버깅 코드
-        #     print(i)
+            # print(turn)
+            # for i in self.board:  # 디버깅 코드
+            #     print(i)
+            # print()
 
-        for santa in range(1, self.p + 1):
-            print(self.scores[santa], end=' ')
+        print(*self.scores[1:], end=' ')
 
 problem = Main()
 problem.solve()
