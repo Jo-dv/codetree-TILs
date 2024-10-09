5697183ef126416d9e92fcde264c80521edb8e3b
WOOJAE JO <63555689+Jo-dv@users.noreply.github.com>
2024-10-10 01:00:49 +0900

    Title: 포탑 부수기, Time: 199ms, Memory: 25MB, Status: Wrong Answer - CodeTree

--- 241010/포탑 부수기/destroy-the-turret.py
+++ 241010/포탑 부수기/destroy-the-turret.py
@@ -7,6 +7,7 @@ class Main:
         self.grid = [list(map(int, input().split())) for _ in range(self.n)]
         self.recent = [[0] * self.m for _ in range(self.n)]  # 마지막 공격 시점
         self.destroy = [[False] * self.m for _ in range(self.n)]  # 파괴된 포탑
+        self.candidates = set()  # 회복 대상에서 제외할 포탑들
         self.answer = 0
 
     def init_game(self):  # 초기 부숴진 포탑 색출
@@ -17,7 +18,7 @@ class Main:
 
     def find_weak(self):  # 약한 포탑 탐색
         result = []
-        min_atk = 5000
+        min_atk = 5001
         for y in range(self.n):
             for x in range(self.m):
                 if not self.destroy[y][x]:
@@ -47,11 +48,16 @@ class Main:
         self.grid[atk_y][atk_x] += (self.n + self.m)  # 공격력 보정
         self.recent[atk_y][atk_x] = step  # 공격 시점 갱신
         damage = self.grid[atk_y][atk_x]  # 편의를 위한 공격력 변수
+        self.candidates.add((atk_y, atk_x))
+        self.candidates.add((def_y, def_x))
 
+        if not self.leaser(atk_y, atk_x, def_y, def_x, damage):
+            self.bomb(atk_y, atk_x, def_y, def_x, damage)
+
+    def leaser(self, atk_y, atk_x, def_y, def_x, damage):
         visited = [[[] for _ in range(self.m)] for _ in range(self.n)]
         dq = deque([(atk_y, atk_x)])
         visited[atk_y][atk_x] = (atk_y, atk_x)
-        candidates = {(atk_y, atk_x), (def_y, def_x)}  # 회복 대상에서 제외할 포탑들
 
         while dq:
             y, x = dq.popleft()
@@ -60,9 +66,9 @@ class Main:
                 while True:
                     y, x = visited[y][x]
                     if (y, x) == (atk_y, atk_x):
-                        return candidates
+                        return True
                     self.grid[y][x] = max(0, self.grid[y][x] - (damage // 2))
-                    candidates.add((y, x))
+                    self.candidates.add((y, x))
 
             for dy, dx in ((0, 1), (1, 0), (-1, 0), (0, -1)):
                 my, mx = (y + dy) % self.n, (x + dx) % self.m
@@ -70,7 +76,9 @@ class Main:
                     visited[my][mx] = (y, x)
                     dq.append((my, mx))
 
-        candidates = {(atk_y, atk_x), (def_y, def_x)}
+        return False
+                    
+    def bomb(self, atk_y, atk_x, def_y, def_x, damage):
         y, x = def_y, def_x
         self.grid[y][x] = max(0, self.grid[y][x] - damage)
         for dy, dx in ((0, 1), (1, 0), (-1, 0), (0, -1), (-1, -1), (-1, 1), (1, -1), (1, 1)):
@@ -78,9 +86,7 @@ class Main:
             if (my, mx) == (atk_y, atk_x):  # 자기 자신은 범위에서 제외
                 continue
             self.grid[my][mx] = max(0, self.grid[my][mx] - (damage // 2))
-            candidates.add((my, mx))
-
-        return candidates
+            self.candidates.add((my, mx))
 
     def check_destroy(self):
         for y in range(self.n):
@@ -88,10 +94,10 @@ class Main:
                 if self.grid[y][x] == 0:
                     self.destroy[y][x] = True
 
-    def recover(self, survivor):  # 무관한 포탑 회복
+    def recover(self):  # 무관한 포탑 회복
         for y in range(self.n):
             for x in range(self.m):
-                if (y, x) not in survivor and not self.destroy[y][x]:
+                if (y, x) not in self.candidates and not self.destroy[y][x]:
                     self.grid[y][x] += 1
 
     def check_terminate(self):
@@ -107,11 +113,11 @@ class Main:
         self.init_game()
 
         for step in range(1, self.k + 1):
-            survivor = self.attack(step)
+            self.attack(step)
             self.check_destroy()
             if self.check_terminate():
                 break
-            self.recover(survivor)
+            self.recover()
 
         for i in self.grid:
             self.answer = max(self.answer, max(i))
