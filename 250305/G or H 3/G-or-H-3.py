b40d2c33ea408602ac2122179b92544561bf084c
WOOJAE JO <63555689+Jo-dv@users.noreply.github.com>
2025-03-05 20:29:51 +0900

    Title: G or H 3, Time: 44ms, Memory: 16MB, Status: Passed - CodeTree

--- 250305/G or H 3/G-or-H-3.py
+++ 250305/G or H 3/G-or-H-3.py
@@ -1,23 +1,19 @@
 n, k = map(int, input().split())
-x = []
-c = []
 temp = []
 for _ in range(n):
     pos, char = input().split()
-    x.append(int(pos))
-    c.append(1 if char == "G" else 2)
     temp.append((int(pos), 1 if char == "G" else 2))
 
 # Please write your code here.
 temp.sort(key=lambda x: x[0])
 answer = 0
 low = 0
+score = 0
+
 for high in range(len(temp)):
-    if temp[high][0] - temp[low][0] <= k:
-        score = 0
-        for i in range(low, high + 1):
-            score += temp[i][1]
-    else:
+    score += temp[high][1]
+    while temp[high][0] - temp[low][0] > k:
+        score -= temp[low][1]
         low += 1
 
     answer = max(answer, score)
