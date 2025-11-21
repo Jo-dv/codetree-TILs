9013c6595eff83f52056bacb065113863f72af18
WOOJAE JO <63555689+Jo-dv@users.noreply.github.com>
2025-11-21 19:45:13 +0900

    Title: DateTime to DateTime, Time: 44ms, Memory: 16MB, Status: Passed - Codetree

---
+++ 251121/DateTime to DateTime/datetime-to-datetime.py
@@ -0,0 +1,7 @@
+a, b, c = map(int, input().split())
+
+# Please write your code here.
+start = 11 * 24 * 60 + 11 * 60 + 11
+end = a * 24 * 60 + b * 60 + c
+result = end - start
+print(result if result >= 0 else -1)
