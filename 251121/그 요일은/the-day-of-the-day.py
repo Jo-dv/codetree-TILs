96cfdc2e33cb8ed18159ab7f136ceccd65234724
WOOJAE JO <63555689+Jo-dv@users.noreply.github.com>
2025-11-21 21:43:34 +0900

    Title: 그 요일은, Time: 45ms, Memory: 16MB, Status: Wrong Answer - Codetree

--- 251121/그 요일은/the-day-of-the-day.py
+++ 251121/그 요일은/the-day-of-the-day.py
@@ -6,14 +6,15 @@ date = {1: 31, 2: 29, 3: 31, 4: 30, 5: 31, 6: 30, 7: 31, 8: 31, 9: 30, 10: 31, 1
 day = {"Mon": 0, "Tue": 1, "Wed": 2, "Thu": 3, "Fri": 4, "Sat": 5, "Sun": 6}
 cnt = 0
 
-result = date[m1] - d1
-for month in range(m1+1, m2):
-    result += date[month]
-result += d2
+if m1 != m2 and d1 != d2:
+    result = date[m1] - d1
+    for month in range(m1+1, m2):
+        result += date[month]
+    result += d2
 
-start = day[A]
-while start <= result:
-    cnt += 1
-    start += 7
+    start = day[A]
+    while start <= result:
+        cnt += 1
+        start += 7
 
 print(cnt)
