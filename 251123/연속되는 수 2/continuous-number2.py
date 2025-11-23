0b46e172b0df9ec5dfd72c1a0663a6783931871e
WOOJAE JO <63555689+Jo-dv@users.noreply.github.com>
2025-11-23 21:39:08 +0900

    Title: 연속되는 수 2, Time: 43ms, Memory: 16MB, Status: Wrong Answer - Codetree

--- 251123/연속되는 수 2/continuous-number2.py
+++ 251123/연속되는 수 2/continuous-number2.py
@@ -6,7 +6,10 @@ answer = 0
 cnt = 0
 
 for i in range(n):
-    if i == 0 or arr[i] != arr[i-1]:
+    if i == 0 or arr[i] == arr[i-1]:
         cnt += 1
+    else:
+        answer = max(answer, cnt)
+        cnt = 1
 
-print(cnt)
+print(answer)
