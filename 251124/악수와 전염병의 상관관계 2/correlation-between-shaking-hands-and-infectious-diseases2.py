662e203b79b26060e78cdd9deec3dd96ff3a6721
WOOJAE JO <63555689+Jo-dv@users.noreply.github.com>
2025-11-24 12:18:30 +0900

    Title: 악수와 전염병의 상관관계 2, Time: 47ms, Memory: 16MB, Status: Passed - Codetree

--- 251124/악수와 전염병의 상관관계 2/correlation-between-shaking-hands-and-infectious-diseases2.py
+++ 251124/악수와 전염병의 상관관계 2/correlation-between-shaking-hands-and-infectious-diseases2.py
@@ -11,14 +11,20 @@ table[P] = K
 handshakes.sort(lambda i: i[0])
 
 for t, x, y in handshakes:
-    if status[x]:
+    if status[x] and not status[y]:
         if table[x] > 0:
-            if not status[y]:
-                status[y] = 1
-                table[y] = K
-            else:
-                if table[y] > 0:
-                    table[y] -= 1
             table[x] -= 1
+            status[y] = 1
+            table[y] = K
+    elif not status[x] and status[y]:
+        if table[y] > 0:
+            table[y] -= 1
+            status[x] = 1
+            table[x] = K
+    elif status[x] and status[y]:
+        if table[x] > 0:
+            table[x] -= 1
+        if table[y] > 0:
+            table[y] -= 1
 
 print(answer.join(str(i) for i in status[1:]))
