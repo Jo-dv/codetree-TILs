aaf623a029394bcf20a6528f8133206c819ca5f5
WOOJAE JO <63555689+Jo-dv@users.noreply.github.com>
2025-11-24 12:07:22 +0900

    Title: 악수와 전염병의 상관관계 2, Time: 48ms, Memory: 16MB, Status: Wrong Answer - Codetree

---
+++ 251124/악수와 전염병의 상관관계 2/correlation-between-shaking-hands-and-infectious-diseases2.py
@@ -0,0 +1,21 @@
+N, K, P, T = map(int, input().split())
+handshakes = [tuple(map(int, input().split())) for _ in range(T)]
+
+# Please write your code here.
+answer = ""
+status = [0] * (N+1)
+status[P] = 1
+table = [0] * (N+1)
+table[P] = K
+
+handshakes.sort(lambda i: i[0])
+
+for t, x, y in handshakes:
+    if status[x]:
+        if table[x] > 0:
+            if not status[y]:
+                status[y] = 1
+                table[y] = K
+            table[x] -= 1
+
+print(answer.join(str(i) for i in status[1:]))
