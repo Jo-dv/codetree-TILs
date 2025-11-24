06f3fa098045b20d5222eecb5e7c1d03eb1d6362
WOOJAE JO <63555689+Jo-dv@users.noreply.github.com>
2025-11-24 09:30:11 +0900

    Title: 벌금은 누구에게, Time: 77ms, Memory: 18MB, Status: Wrong Answer - Codetree

---
+++ 251124/벌금은 누구에게/who-will-pay.py
@@ -0,0 +1,11 @@
+N, M, K = map(int, input().split())
+student = [int(input()) for _ in range(M)]
+
+# Please write your code here.
+info = [0] * (N + 1)
+
+for i in student:
+    info[i] += 1
+    if info[i] >= K:
+        print(i)
+        break
