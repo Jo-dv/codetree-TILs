f67706c6bf56924e310f45aa5c64267bab4c1dbb
WOOJAE JO <63555689+Jo-dv@users.noreply.github.com>
2025-12-02 10:41:12 +0900

    Title: 1차원 폭발 게임, Time: 1087ms, Memory: 18MB, Status: Time Limit Exceed - Codetree

---
+++ 251202/1차원 폭발 게임/The-1D-bomb-game.py
@@ -0,0 +1,28 @@
+n, m = map(int, input().split())
+numbers = [int(input()) for _ in range(n)]
+
+# Please write your code here.
+while True:
+    temp = []
+    low = 0
+    high = 1
+    while high < len(numbers):
+        if numbers[low] == numbers[high]:
+            high += 1
+        else:
+            if high - low >= m:
+                for i in range(low, high):
+                    temp.append(i)
+            low = high
+            high += 1
+    else:
+        if high - low >= m:
+            for i in range(low, high):
+                temp.append(i)
+    if not temp:
+        break
+    numbers = [numbers[i] for i in range(len(numbers)) if i not in temp]
+
+print(len(numbers))
+for i in numbers:
+    print(i)
