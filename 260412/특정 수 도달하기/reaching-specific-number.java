7bc9be6eec0ef4bd506d333b2192f52bbd2b30a6
WOOJAE JO <63555689+Jo-dv@users.noreply.github.com>
2026-04-12 21:46:01 +0900

    Title: 특정 수 도달하기, Time: 0ms, Memory: 0MB, Status: Passed - Codetree

--- 260412/특정 수 도달하기/reaching-specific-number.java
+++ 260412/특정 수 도달하기/reaching-specific-number.java
@@ -6,24 +6,34 @@ public class Main {
     static StringTokenizer st;
     static StringBuilder sb = new StringBuilder();
     static int sum = 0;
-    static double avg = 0;
     static boolean flag = false;
     static double cnt = 0;
 
     public static void main(String[] args) throws IOException {
         st = new StringTokenizer(br.readLine());
-        for(int i = 0; i < 10; i++) {
+
+        for (int i = 0; i < 10; i++) {
             int n = Integer.parseInt(st.nextToken());
+
             if (n >= 250) {
                 flag = true;
-                 break;
+                break;
             }
 
             sum += n;
-            cnt += 1;
+            cnt++;
         }
 
-        sb.append(sum).append(" ").append(flag ? sum / cnt : sum / 10.0);
+        double avg;
+
+        if (flag) {
+            avg = sum / cnt;
+        } else {
+            avg = sum / 10.0;
+        }
+
+        avg = Math.round(avg * 10) / 10.0;
+        sb.append(sum).append(" ").append(avg);
         System.out.println(sb);
     }
 }
