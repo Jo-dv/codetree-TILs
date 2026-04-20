e2609f9074e3a71d93b31a8554d2d359778de90b
WOOJAE JO <63555689+Jo-dv@users.noreply.github.com>
2026-04-20 19:01:34 +0900

    Title: 마라톤 중간에 택시타기 2, Time: 0ms, Memory: 0MB, Status: Time Limit Exceed - Codetree

--- 260420/마라톤 중간에 택시타기 2/taking-a-taxi-in-the-middle-of-the-marathon-2.java
+++ 260420/마라톤 중간에 택시타기 2/taking-a-taxi-in-the-middle-of-the-marathon-2.java
@@ -6,7 +6,6 @@ public class Main {
     static StringTokenizer st;
     static Node[] arr;
     static int n;
-    static boolean[] visited;
     static int answer = Integer.MAX_VALUE;
 
     public static class Node {
@@ -18,15 +17,15 @@ public class Main {
         }
     }
 
-    public static void search(int prev, int nxt, int cnt, int distance) {
-        if(nxt == n-1) {
-            if(cnt == n-1) {
-                answer = Math.min(answer, distance);
-            }
-            return;
+public static void search(int prev, int nxt, int cnt, int distance) {
+    if(nxt == n-1) {
+        if(cnt == n-1) {
+            answer = Math.min(answer, distance);
         }
+        return;
+    }
 
-        for(int i = nxt + 1; i < n; i++) {
+    for(int i = nxt + 1; i <= nxt + 2 && i < n; i++) {
             int new_distance = Math.abs(arr[nxt].x - arr[i].x) + Math.abs(arr[nxt].y - arr[i].y);
             search(nxt, i, cnt + 1, distance + new_distance);
         }
@@ -36,7 +35,6 @@ public class Main {
         // Please write your code here.
         n = Integer.parseInt(br.readLine());
         arr = new Node[n];
-        visited = new boolean[n];
 
         for(int i = 0; i < n; i++) {
             st = new StringTokenizer(br.readLine());
@@ -46,8 +44,6 @@ public class Main {
             arr[i] = node;
         }
 
-        visited[0] = true;
-        visited[n-1] = true;
         for(int i = 1; i < n; i++) {
             int initial_dist = Math.abs(arr[0].x - arr[i].x) + Math.abs(arr[0].y - arr[i].y);
             search(0, i, 2, initial_dist);
