ee5bc5a777032d71f4b91916daa617c0099d7d29
WOOJAE JO <63555689+Jo-dv@users.noreply.github.com>
2026-07-27 16:54:36 +0900

    [Passed] 방향에 맞춰 최대로 움직이기 (64ms, 21MB)

--- 260727/방향에 맞춰 최대로 움직이기/max-movements-with-direction.py
+++ 260727/방향에 맞춰 최대로 움직이기/max-movements-with-direction.py
@@ -3,15 +3,11 @@ grid = [list(map(int, input().split())) for _ in range(n)]
 arrow = [list(map(int, input().split())) for _ in range(n)]
 directions = {1: (-1, 0), 2: (-1, 1), 3: (0, 1), 4: (1, 1), 5: (1, 0), 6: (1, -1), 7: (0, -1), 8: (-1, -1)}
 sy, sx = map(lambda x: int(x) - 1, input().split())
-
-visited = [[False] * n for _ in range(n)]
-visited[sy][sx] = True
 answer = 0
 
-def search(y, x, visited, cnt):
+def search(y, x, cnt):
     global answer
-    if cnt > 0:
-        answer = max(answer, cnt)
+    answer = max(answer, cnt)
 
     d = arrow[y][x]
     dy, dx = directions[d]
@@ -20,11 +16,9 @@ def search(y, x, visited, cnt):
 
     while 0 <= my < n and 0 <= mx < n:
         if grid[y][x] < grid[my][mx]:
-            visited[my][mx] = True
-            search(my, mx, visited, cnt+1)
-            visited[my][mx] = False
+            search(my, mx, cnt+1)
         my += dy
         mx += dx
 
-search(sy, sx, visited, 0)
+search(sy, sx, 0)
 print(answer)
