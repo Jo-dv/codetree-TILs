609baad6bc82d93aeff306576a7911350839646a
WOOJAE JO <63555689+Jo-dv@users.noreply.github.com>
2026-08-06 20:29:30 +0900

    [Passed] 상해버린 치즈 (57ms, 16MB)

--- 260806/상해버린 치즈/rotten-cheese.py
+++ 260806/상해버린 치즈/rotten-cheese.py
@@ -1,50 +1,39 @@
-import sys
-
-input = sys.stdin.readline
-
 N, M, D, S = map(int, input().split())
 
-# eat_records[치즈] = [(사람, 먹은 시각), ...]
-eat_records = [[] for _ in range(M + 1)]
-
+p, m, t = [], [], []
 for _ in range(D):
-    person, cheese, eat_time = map(int, input().split())
-    eat_records[cheese].append((person, eat_time))
+    person, milk, time = map(int, input().split())
+    p.append(person)
+    m.append(milk)
+    t.append(time)
 
-sick_records = []
-
+sick_p, sick_t = [], []
 for _ in range(S):
-    person, sick_time = map(int, input().split())
-    sick_records.append((person, sick_time))
+    person, time = map(int, input().split())
+    sick_p.append(person)
+    sick_t.append(time)
 
+# Please write your code here.
 answer = 0
+eat_list = {i: [] for i in range(1, M+1)}
+for i in range(D):
+    eat_list[m[i]].append([p[i], t[i]])
 
-# 각 치즈를 상한 치즈라고 가정
-for bad_cheese in range(1, M + 1):
-    possible = True
-
-    # 아픈 모든 사람이 아프기 전에 이 치즈를 먹었는지 확인
-    for sick_person, sick_time in sick_records:
-        ate_before_sick = False
-
-        for person, eat_time in eat_records[bad_cheese]:
-            if person == sick_person and eat_time < sick_time:
-                ate_before_sick = True
+for cheese in range(1, M+1):
+    candidate = True
+    for sick_person, sick_time in zip(sick_p, sick_t):
+        sick = False
+        for person, time in eat_list[cheese]:
+            if person == sick_person and time < sick_time:
+                sick = True
                 break
-
-        if not ate_before_sick:
-            possible = False
+                
+        if not sick:
+            candidate = False
             break
-
-    if not possible:
-        continue
-
-    # 해당 치즈를 한 번이라도 먹은 사람 수
-    people = set()
-
-    for person, eat_time in eat_records[bad_cheese]:
-        people.add(person)
-
-    answer = max(answer, len(people))
+        
+    if candidate:
+        people = set(p for p, t in eat_list[cheese])
+        answer = max(answer, len(people))
 
 print(answer)
